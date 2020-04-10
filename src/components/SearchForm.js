32b99f5fe9e1a62d215cadff6806758298f765e2
dlarner3194 <dlarner3194@gmail.com>
2020-04-10 08:51:53 -0400

    adds handle submit

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,32 +1,96 @@
 import React from "react";
 
+const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
+
 class SearchForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    this.state = {
+      query: "",
+      error: null,
+      loading: false,
+      items: [],
+      message: "",
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    this.setState({ query: event.target.value });
   }
 
   handleSubmit(event) {
-    alert("A song was submitted: " + this.state.value);
+    const query = event.target.query.value;
     event.preventDefault();
+    if (!query) {
+      this.setState({
+        query,
+        items: [],
+        message: "",
+        totalPages: 0,
+        totalResults: 0,
+      });
+    } else {
+      this.setState({ query, loading: true, message: "" }, () => {
+        fetch(`${apiEndpoint}/search?track=${query}`)
+          .then((res) => res.json())
+          .then(
+            (result) => {
+              console.log(result);
+              this.setState({
+                loading: false,
+                items: result,
+              });
+            },
+            (error) => {
+              console.log(error);
+              this.setState({
+                loading: false,
+                error,
+              });
+            }
+          );
+      });
+    }
+
+    this.setState({ query, loading: true });
   }
 
   render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          <input type="text" name="name" placeholder="Enter a song name" />
-        </label>
-        <input type="submit" value="Search" />
-      </form>
-    );
+    const { query, error, loading, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (loading) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit} htmlFor="search-input">
+            <label>
+              Track:
+              <input
+                type="text"
+                name="query"
+                id="search-input"
+                value={query}
+                placeholder="Search..."
+                onChange={this.handleChange}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          {items.map((item) => (
+            <ul style={{ listStyleType: "none" }}>
+              <li key={item.name}>Name: {item.name}</li>
+              <li key={item.album.name}>Album name: {item.album.name}</li>
+              <li key={item.duration_ms}>Duration: {item.duration_ms}</li>
+            </ul>
+          ))}
+        </div>
+      );
+    }
   }
 }
 
